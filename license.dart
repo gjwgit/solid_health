be27f3621cfe20973fec2fcc98f77322faa07eaf
atangster <ashley.t1248@gmail.com>
2025-01-06 11:45:20 +1100

    fix: reset license template

--- license.dart
+++ license.dart
@@ -1,4 +1,4 @@
-/// HealthPod License
+/// <DESCRIPTION>
 //
 // Time-stamp: <Thursday 2024-12-19 13:33:06 +1100 Graham Williams>
 //
@@ -21,6 +21,6 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <https://www.gnu.org/licenses/>.
 ///
-/// Authors: Ashley Tang
+/// Authors: <AUTHORS>
 
 library;
